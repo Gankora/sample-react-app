c94e4afec2d4b0a1084d7692b1fdcb01c4eed358
Kostya Kryukaev <kryukaev.kostya@yandex.ru>
2024-08-22 18:30:31 +0300

    changed app.js without JSX in the main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
+import { createElement } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-export const App = () => {
-	return (
-		// в данном файле применяется декларативнй стиль программирования формализующий структуру элементов, без указания, какие процессы они должны выполнить.
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<p>{new Date().getFullYear()}</p>
-			</header>
-		</div>
-	);
-};
+export function App() {
+  return createElement(
+    "div",
+    { className: "App" },
+    createElement(
+      "header",
+      { className: "App-header" },
+      createElement("img", { src: logo, className: "App-logo", alt: "logo" }),
+      createElement(
+        "p",
+        null,
+        "Edit ",
+        createElement("code", null, "src/App.js"),
+        " and save to reload."
+      ),
+      createElement(
+        "a",
+        {
+          className: "App-link",
+          href: "https://reactjs.org",
+          target: "_blank",
+          rel: "noopener noreferrer",
+        },
+        "Learn React"
+      ),
+      createElement("p", null, new Date().getFullYear())
+    )
+  );
+}
